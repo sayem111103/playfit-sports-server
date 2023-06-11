09197c1b49c2b1783476ff8167990f44464f87c5
sayem111103 <sayemul03@gmail.com>
2023-06-11 15:04:06 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middlewares 
+// middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Welcome to playfit sports');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to playfit sports");
+});
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(401).send({error:true, message:'unauthorized access'})
+    }
+    req.decoded = decoded
+    next()
+  });
+};
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.glhjkho.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,26 +38,77 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const classCollection = client.db('Playfit-Sports').collection('classes');
-    const instructorCollection = client.db('Playfit-Sports').collection('instructors');
+    const classCollection = client.db("Playfit-Sports").collection("classes");
+    const instructorCollection = client
+      .db("Playfit-Sports")
+      .collection("instructors");
+    const usersCollection = client.db('Playfit-Sports').collection("users")
 
-    app.get('/classes', async(req, res)=>{
-        const result = await classCollection.find().toArray();
-        res.send(result);
-    })
-    app.get('/instructors', async(req, res) =>{
+    const verifyAdmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user.role !== 'admin'){
+        return res.status(403).send({error:true, message:'forbidden message'})
+      }
+      next()
+    }
+
+    const verifyInstructor = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user.role !== 'instructor'){
+        return res.status(403).send({error:true, message:'forbidden message'})
+      }
+      next()
+    }  
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res.send({ token });
+    });
+
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructors", async (req, res) => {
       const result = await instructorCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await instructorCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/user', async(req, res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const exist = await usersCollection.findOne(query);
+      if(exist){
+        return res.send({message: 'already exist'})
+      }
+      const result = await usersCollection.insertOne(user);
       res.send(result)
     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
